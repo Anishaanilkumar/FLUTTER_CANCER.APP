ebbdcce32b605ac4c054d6def9664285ceaa30de
Anishaanilkumar <158391294+Anishaanilkumar@users.noreply.github.com>
2024-08-25 00:12:48 +0530

    created a home and added a title text inside the body just as an example

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Text("anisha"),
+    );
   }
 }
